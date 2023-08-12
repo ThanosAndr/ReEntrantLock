82e006a240143766edaed096f22449bc4e0e5136
ThanosAndr <andriopoulosthanassis@gmail.com>
2023-08-12 19:06:25 +0300

    Enables CTest and removes redundant unlock calls.

--- libs/ReEntrantLock/src/ReEntrantLock.cpp
+++ libs/ReEntrantLock/src/ReEntrantLock.cpp
@@ -5,7 +5,6 @@ void ReentrantLock::lock() {
 		if(m_count > 0) {
 			if(m_owner_id == std::this_thread::get_id()) {
 				++m_count;
-				lk.unlock();
 				return;
 			} else {
 				while (m_count > 0) {
@@ -15,21 +14,17 @@ void ReentrantLock::lock() {
 		}
 		m_owner_id = std::this_thread::get_id();
 		m_count = 1;
-		lk.unlock();
 }
 
 void ReentrantLock::unlock() {
 		std::unique_lock<std::mutex> lk(m_mutex);
 		if (m_count == 0) {
-			lk.unlock();
 			throw std::logic_error("Already Unlocked");
 		}
 		if(m_owner_id !=  std::this_thread::get_id()) {
-			lk.unlock();
 			throw std::logic_error("Unlock called by non-owner thread");
 		}
 		if (--m_count == 0) {
-			lk.unlock();
 			m_cv.notify_all();
 		} 
 	}
